54c40cf3d7f4a0453f07ee6541b6e23c4eccaff0
Will Papper <contact@papper.me>
2022-11-02 14:48:30 +0100

    Update Solidity version and import VRGDAs

--- src/Aminal.sol
+++ src/Aminal.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.17;
 
 import "openzeppelin/token/ERC721/ERC721.sol";
+import "../lib/VRGDAs/src/LinearVRGDA.sol";
 
 error AminalDoesNotExist();
 error SenderDoesNotHaveMaxAffinity();
